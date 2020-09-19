3925b246bfa6a816c6e3fea42fc28dfc1c778e60
7vikpeculiar <bugganasathviksanjeev1998@gmail.com>
2020-09-19 12:44:06 +0530

    Added main

--- main.jl
+++ main.jl
@@ -1,6 +1,6 @@
 include("./regex.jl")
 using .Regex
-# println(matchStart("a","a"));
-# println(matchReg("abc","abc\$"));
-# println(searchReg("^abc","abcd"));
-# println(matchReg("c\$","cdd"));
+println(matchStart("a","a"));
+println(matchReg("abc","abc\$"));
+println(searchReg("^abc","abcd"));
+println(matchReg("c\$","cdd"));
